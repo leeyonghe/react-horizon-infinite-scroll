961d02bc74ce398aa68a918990aec5466a46d834
david <lyhdra99@gmail.com>
2025-04-28 19:54:13 +0900

    chore: 예제 프로젝트 의존성 및 페이지 컴포넌트 업데이트

--- example/src/app/page.tsx
+++ example/src/app/page.tsx
@@ -10,7 +10,6 @@ export default function Home() {
   const obj = [
     {
       imgURL: '/banner01.png',
-      imgURL2: '/banner01_mo.png',
       imgAlt: 'img-1',
       isMovie: false,
       title: '외국어 테스트',
@@ -20,7 +19,6 @@ export default function Home() {
     },
     {
       imgURL: '/movie.mp4',
-      imgURL2: '/movie.mp4',
       isMovie: true,
       imgAlt: 'img-2',
       title: '이건 한국어 테스트',
@@ -31,7 +29,7 @@ export default function Home() {
   ];
   const [isHorizontal, setIsHorizontal] = useState(true);
 
-  function makeExplainHtml(obj: { imgURL: string; imgURL2: string; imgAlt: string; isMovie: boolean; title: string; subtitle: string; link: string; logo: string; }): import("react").ReactNode {
+  function makeExplainHtml(obj: { imgURL: string; imgAlt: string; isMovie: boolean; title: string; subtitle: string; link: string; logo: string; }): import("react").ReactNode {
     return <div style={{ position: 'absolute', display: 'flex', flexDirection: 'column', }}>
       <Image src={`${obj.logo}`}
         className=""
@@ -52,17 +50,6 @@ export default function Home() {
 
   return (
     <div className="fixed inset-0 w-full h-full overflow-hidden">
-      {/* <div className="mb-4 p-4">
-        <label className="flex items-center space-x-2">
-          <input
-            type="checkbox"
-            checked={isHorizontal}
-            onChange={(e) => setIsHorizontal(e.target.checked)}
-            className="form-checkbox h-5 w-5 text-blue-600"
-          />
-          <span>Horizontal Scroll</span>
-        </label>
-      </div> */}
       <InfiniteScroll
         height={500}
         isHorizontal={isHorizontal}
